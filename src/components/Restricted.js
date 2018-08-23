da3343bbb1344ec8d59078b9cbb69c2e516ed8d8
moshed <mdavidesko@gmail.com>
2018-08-24 02:09:02 +0300

    add auth in route

---
+++ src/components/Restricted.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { login, logout, isLoggedIn } from '../utils/AuthService';
+import Button from '@material-ui/core/Button';
+import Img from 'react-image'
+
+class Restricted extends Component {
+
+
+  render() {
+    const styles= {
+      image:{
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop:"5%",
+        width: "50%",
+    
+      },
+      login:{
+        marginLeft: "auto",
+        marginRight: "auto",
+        display: "block",
+        
+      }
+
+    }
+
+    return (
+      <div >
+        <Button style={styles.login} onClick={login} variant="contained" color="primary">Please Login</Button>
+        <Img style={styles.image} src="https://www.seton.com/media/catalog/product/cache/4/image/85e4522595efc69f496374d01ef2bf13/1435185270/r/e/restricted-area-security-stop-signs-m9081-lg.png" />
+      </div>
+    );
+  }
+}
+
+export default Restricted;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Upload from './components/Upload';
+import Restricted from './components/Restricted';
 import Display from './components/Display';
 import DisplayByName from './components/DisplayByName';
 import SelectChild from './components/SelectChild';
@@ -11,6 +12,8 @@ import { Switch,Router, Route , BrowserRouter} from 'react-router-dom'
 import { requireAuth } from './utils/AuthService';
 import history from './components/history';
 import AppBar from './components/AppBar';
+import { AuthRoute } from 'react-router-auth';
+import { login, logout, isLoggedIn } from './utils/AuthService';
 
 
 const Root = () => {
@@ -20,11 +23,14 @@ const Root = () => {
         <Router history={history}>
         <div>
           <AppBar/>
+          <AuthRoute path="/select" component={SelectChild} redirectTo="/restricted" authenticated={isLoggedIn()} />
+          <AuthRoute path="/name/:username" component={DisplayByName} redirectTo="/restricted" authenticated={isLoggedIn()} />          
           <Route exact path="/" component={Home}/>
-          <Route path="/name/:username" component={DisplayByName}/>
-          <Route path="/select" component={SelectChild}/>                                                        
+          <Route exact path="/restricted" component={Restricted}/>          
+          {/* <Route path="/name/:username" component={DisplayByName} /> */}
+          {/* <Route path="/select" component={SelectChild}/>                                                         */}
           <Route path="/upload" component={Upload} onEnter={requireAuth} />
-          <Route path="/all" component={Display} onEnter={requireAuth} />          
+          {/* <Route path="/all" component={Display} onEnter={requireAuth} />           */}
           <Route path="/callback" component={Callback} onEnter={requireAuth} />
         </div>
         </Router>
